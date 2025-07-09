2409c7d4f5fb759776f8edd689e71b97a9317420
JRBashL <jrbashl.dev@gmail.com>
2025-07-09 00:05:13 -0600

    Code updates:
    Added new code to Pipe1BrokenScript that corresponds to launching the broken pipe child according to the collision.
    Created an OnCollisionEnter function that applies a AddForceAtPosition where the Bord hit the broken pipe.
    Added flags to make sure force is only applied one time to avoid funny behaviour.
    
    Also made the Bord have a trigger collider and a physics collider. Both are child of the GameObject.
    
    Next time, need to add a broken pipe base script to continue the movement of the pipe base.
    Also need a despawning system for the broken pipes.

--- Assets/Scripts/Pipe/Pipe1BrokenScript.cs
+++ Assets/Scripts/Pipe/Pipe1BrokenScript.cs
@@ -4,23 +4,57 @@ public class Pipe1BrokenScript : MonoBehaviour
 {
 
     private Rigidbody _rb;
+    private CapsuleCollider _ownCollider;
+    private Vector3 _collisionLocation;
+    private bool _ishit;
 
     void Awake()
     {
         // Get the Rigidbody component attached to this GameObject
         _rb = gameObject.GetComponent<Rigidbody>();
+        _ownCollider = gameObject.GetComponent<CapsuleCollider>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!_ishit)
+        {
+            transform.Translate(0, 0, PipePrefabScript.PipeSpeed * Time.deltaTime);
+        }
         
-        transform.Translate(0, 0, PipePrefabScript.PipeSpeed * Time.deltaTime);
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Pipe1Brokenscript detected collision with " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Bord") && !_ishit)
+        {
+            _ishit = true;
+            _rb.useGravity = true;
+
+            Debug.Log("Pipe1Brokenscript detected collision with Bord");
+
+            _collisionLocation = collision.transform.position;
+            _rb.AddForceAtPosition(
+                new Vector3(Random.Range(-75, 75), Random.Range(-75, 75), Random.Range(50, 100)),
+                _collisionLocation,
+                ForceMode.Impulse);
+
+            _ownCollider.enabled = false;
+
+            
+        }
+    }
+
+
+
 }
+
