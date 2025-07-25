a5ea6d4ef66c10ad6c09bcb193843182ed8ee477
JRBashL <jrbashl.dev@gmail.com>
2025-07-25 15:36:21 -0600

    Created Jump FSM Scriptable Object

---
+++ Assets/Scriptable Objects/JumpStateMachineSO.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "JumpStateMachine", menuName = "Scriptable Objects/JumpStateMachine")]
+public class JumpStateMachineSO : ScriptableObject
+{
+
+    // FSM for Bord Jumping
+    public enum JumpFSM { NotJumping, RegularSpeedJumping, SpeedBoostJumping }
+    [SerializeField] private JumpFSM _jumpState;
+
+    // Property
+    public JumpFSM JumpState
+    {
+        get
+        {
+            return _jumpState;
+        }
+
+        set
+        {
+            if (!Enum.IsDefined(typeof(JumpFSM), value))
+            {
+                Debug.LogError("Setting wrong value for JumpFSM. Setting to NotJumping");
+                _jumpState = JumpFSM.NotJumping;
+            }
+
+            else
+            {
+                _jumpState = value;
+            }
+        }
+    }
+
+    // Set default starting value.
+    void OnEnable()
+    {
+        _jumpState = JumpFSM.NotJumping;
+    }
+
+}
