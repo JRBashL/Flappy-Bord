6070b9b6776ca6a46b4b07783fa46134c81bf601
JRBashL <jrbashl.dev@gmail.com>
2025-07-25 02:25:28 -0600

    Refactor BordCollision to use BordSOFSM

--- Assets/Scripts/Bord/BordCollision.cs
+++ Assets/Scripts/Bord/BordCollision.cs
@@ -1,9 +1,15 @@
-using Unity.VisualScripting;
 using UnityEngine;
+// Shorthanding the external script for the enum
+using BordSOFSM = BordStateMachineSO.BordMainFSM;
 
 // Script is handling the logic of collisions involved with the Bord
 public class BordCollision : MonoBehaviour
 {
+
+    //Drag instance of BordFSMSO
+    [SerializeField]
+    private BordStateMachineSO _bordStateSO;
+
     private Rigidbody _bordRigidbody;
     private GameObject _otherGameObject;
     // Script dependent on reading the BordStateMachine enum
@@ -12,19 +18,8 @@ public class BordCollision : MonoBehaviour
     void Awake()
     {
         _bordRigidbody = GetComponent<Rigidbody>();
-        _bordState = GetComponent<BordStateMachine>();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     
     void OnTriggerEnter(Collider Col)
     {
@@ -33,14 +28,14 @@ public class BordCollision : MonoBehaviour
         // Code is written in this way in case the collider is a child.       
         _otherGameObject = Col.gameObject.transform.root.gameObject;
 
-        switch (_bordState.CurrentBordState)
+        switch (_bordStateSO.BordMainState)
         {
-            case BordStateMachine.BordStateEnum.BeginState:
+            case BordSOFSM.BeginState:
 
                 // Do nothing
                 break;
 
-            case BordStateMachine.BordStateEnum.RegularSpeedState:
+            case BordSOFSM.RegularSpeedState:
 
                 if (Col.gameObject.name == "Gap Collider")
                 {
@@ -51,8 +46,8 @@ public class BordCollision : MonoBehaviour
                 {
                     Debug.Log("Bord Collided with the Pipes");
 
-                    // Changes the coroutine state of the BordStateMachine
-                    _bordState.StartDeadState();
+                    // Changes the state using the SOFSM
+                    _bordStateSO.BordMainState = BordSOFSM.DeadState;
 
                     // Makes the Bord a dynamic rigidbody and adds a random force vector.
                     _bordRigidbody.isKinematic = false;
@@ -65,13 +60,32 @@ public class BordCollision : MonoBehaviour
                     );
                 }
 
+                else if (Col.CompareTag("Ground"))
+                {
+                    Debug.Log("Bord Collided with the Pipes");
+
+                    // Changes the state using the SOFSM
+                    _bordStateSO.BordMainState = BordSOFSM.DeadState;
+
+                    // Makes the Bord a dynamic rigidbody and adds a random force vector.
+                    _bordRigidbody.isKinematic = false;
+                    _bordRigidbody.AddForce(
+                        new Vector3(Random.Range(-2f, 2f),
+                                    Random.Range(0f, 20f),
+                                    Random.Range(-2f, 2f)
+                        ),
+                        ForceMode.Impulse
+                    );
+                }
+
                 break;
 
-            case BordStateMachine.BordStateEnum.BoostSpeedState:
+            case BordSOFSM.BoostSpeedState:
                 // Do Nothing. Maybe this can trigger some particles in the future.
-
                 break;
-            case BordStateMachine.BordStateEnum.DeadState:
+
+            case BordSOFSM.DeadState:
+
                 // Do nothing       
                 break;
         }
