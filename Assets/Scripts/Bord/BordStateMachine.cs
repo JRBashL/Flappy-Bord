f8297c52cb0766f9002f78aaddad70efe6dd6f20
JRBashL <jrbashl.dev@gmail.com>
2025-08-03 20:40:25 -0600

    Removed start begin state at start method

--- Assets/Scripts/Bord/BordStateMachine.cs
+++ Assets/Scripts/Bord/BordStateMachine.cs
@@ -21,7 +21,7 @@ public class BordStateMachine : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartBeginState();
+
     }
 
     // Update is called once per frame
